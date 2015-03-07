9db49befd3fe4a5f39699c11c4fcd1a5ea16e51f
unknown <bret.maxime@gmail.com>
2015-03-07 18:49:44 +0100

    test

--- config/http.js
+++ config/http.js
@@ -63,9 +63,9 @@ module.exports.http = {
 
     // Add the /data folder to the static asset
     // This path is used to serve content that come from users (this content is not inside assets folder)
-    staticData: express.static(__dirname + '/../data/statics', {
+    //staticData: express.static(__dirname + '/../data/statics', {
         // here is possible options for the static path
-    }),
+    //}),
 
     /**
     * Custom middleware that do some job.
