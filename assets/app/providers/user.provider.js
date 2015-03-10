21d86ffb4b9098492f62f981c9cce8a39e712bbb
Maxime Bret <bret.maxime@gmail.com>
2015-03-10 14:04:39 +0100

    maj

--- assets/app/providers/user.provider.js
+++ assets/app/providers/user.provider.js
@@ -7,6 +7,7 @@
     angular
         .module('app.services')
         .provider('user', userProvider)
+        .factory('User', userService)
         .factory('userService', userService);
     
     function userProvider(){
@@ -16,7 +17,7 @@
         return {
             setData: setData,
             $get: userFactory
-        }
+        };
 
         /**
          * Set the data that will be used to build the user.
@@ -27,126 +28,129 @@
             userData = data;
         }
 
-        userFactory.$inject = ['_', '$injector', 'dataservice', 'localStorageService'];
+        userFactory.$inject = ['User'];
         /**
          * Build a user object that come will useful method.
          * This object represent logged user so there is only one.
          * SINGLETON
          */
-        function userFactory(_, $injector, dataservice, localStorageService) {
-
-            var User = function( data ){
-
-                var self = this;
-                    
-                /*
-                 * Attributes
-                 */
-                self.profiles = angular.copy(data.profiles);
-                // array of settings with their values
-                self.settings = angular.copy(data.settings);
-                self.id = data.id;
-                self.email = data.email;
-                self.backgroundImages = [];
-                self.avatar = data.avatar;
-                self.banner = data.banner;
-                self.profile = data.profile;
-                self.firstName = data.firstName;
-                self.lastName = data.lastName;
-
-                if(data.backgroundImages && Array.isArray(data.backgroundImages)){
-                    self.backgroundImages = angular.copy(data.backgroundImages);
-                }
-                
-                mergeLocal();
-                
-                /*
-                 * Constants
-                 * constants are here to reduce the use of potentially future changed strings.
-                 * Use them inside controller etc and change only here when server changed. 
-                 */
-                User.prototype.CONST = {
-                    SETTING_WIDGETS_BORDERS : 'widgetsBorders',
-                    SETTING_BACKGROUND_IMAGES_INTERVAL : 'backgroundImagesInterval'
-                };
-                
-                User.prototype.addBackgroundImage = function( key ){
-                    self.backgroundImages.push(key);
-                };
-                
-                User.prototype.removeBackgroundImage = function( key ){
-                    self.backgroundImages = _.remove(self.backgroundImages, function(n) {
-                        return key !== n;
-                    });
-                };
-                
-                /**
-                 * Use this function to set new active profile as local.
-                 */
-                User.prototype.setActiveProfile = function( profile ){
-                    self.profile = profile.id;
-                };
-
-                User.prototype.getActiveProfile = function(){
-                    return self.profile;
-                };
-                
-                /**
-                 *
-                 * @param id
-                 * @param value
-                 */
-                User.prototype.setSetting = function( id, value ){
-                    self.settings[id] = value;
-                };
-
-                /**
-                 * @todo depend of the type we should return specific value here
-                 * @param id
-                 * @returns {*}
-                 */
-                User.prototype.getSetting = function( id ){
-                    return self.settings[id];
-                };
-
-                User.prototype.save = function(){
-                    var data = {
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        settings: user.settings,
-                        backgroundImages: user.backgroundImages
-                    };
-                    return dataservice.updateAccount(data).then(function(user){
-                        saveLocal();
-                        return user;
-                    });
-                };
-
-                function saveLocal(){
-                    var data = {
-                        profile: self.profile
-                    };
-                    localStorageService.set('user.' + self.id, data);
-                }
-
-                function mergeLocal(){
-                    var localUser = localStorageService.get('user.' + self.id);
-                    self.profile = localUser.profile;
-                }
-            };
-
-            return new User( userData );
+        function userFactory(User) {
+            return new userService( userData );
         }
     }
 
     userService.$inject = ['localStorageService', 'dataservice'];
     function userService(localStorageService, dataservice){
-        
-        return {
-            
-            
-            
-        }
+
+        var User = function( data ){
+
+            var self = this;
+
+            /*
+             * Attributes
+             */
+            self.profiles = angular.copy(data.profiles);
+            // array of settings with their values
+            self.settings = angular.copy(data.settings);
+            self.id = data.id;
+            self.email = data.email;
+            self.backgroundImages = [];
+            self.avatar = data.avatar;
+            self.banner = data.banner;
+            self.profile = data.profile;
+            self.firstName = data.firstName;
+            self.lastName = data.lastName;
+
+            if(data.backgroundImages && Array.isArray(data.backgroundImages)){
+                self.backgroundImages = angular.copy(data.backgroundImages);
+            }
+
+            mergeLocal();
+
+            /*
+             * Constants
+             * constants are here to reduce the use of potentially future changed strings.
+             * Use them inside controller etc and change only here when server changed.
+             */
+            User.prototype.CONST = {
+                SETTING_WIDGETS_BORDERS : 'widgetsBorders',
+                SETTING_BACKGROUND_IMAGES_INTERVAL : 'backgroundImagesInterval'
+            };
+
+            User.prototype.addBackgroundImage = function( key ){
+                self.backgroundImages.push(key);
+            };
+
+            User.prototype.removeBackgroundImage = function( key ){
+                self.backgroundImages = _.remove(self.backgroundImages, function(n) {
+                    return key !== n;
+                });
+            };
+
+            /**
+             * Use this function to set new active profile as local.
+             */
+            User.prototype.setActiveProfile = function( profile ){
+                self.profile = profile.id;
+            };
+
+            User.prototype.getActiveProfile = function(){
+                return self.profile;
+            };
+
+            /**
+             *
+             * @param id
+             * @param value
+             */
+            User.prototype.setSetting = function( id, value ){
+                self.settings[id] = value;
+            };
+
+            /**
+             * @todo depend of the type we should return specific value here
+             * @param id
+             * @returns {*}
+             */
+            User.prototype.getSetting = function( id ){
+                return self.settings[id];
+            };
+
+            User.prototype.save = function(){
+                var data = {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    settings: user.settings,
+                    backgroundImages: user.backgroundImages
+                };
+                return dataservice.updateAccount(data).then(function(user){
+                    saveLocal();
+                    return user;
+                });
+            };
+
+            function saveLocal(){
+                var data = {
+                    profile: self.profile
+                };
+                localStorageService.set('user.' + self.id, data);
+            }
+
+            function mergeLocal(){
+                var localUser = localStorageService.get('user.' + self.id);
+                self.profile = localUser.profile;
+            }
+        };
+
+        User.find = function(){
+
+        };
+
+        User.findOne = function(){
+
+        };
+
+        return User;
         
     }
 })();
